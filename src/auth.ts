201fb4dba3c09e0d0f486708b34f27a18739c299
S <s@sample.com>
2025-10-18 18:24:31 +0900

    認証 - 利便性 Session から取得しやすく

--- src/auth.ts
+++ src/auth.ts
@@ -38,4 +38,15 @@ export const { auth, signIn, signOut, handlers } = NextAuth({
       },
     }),
   ],
+  // token.subにidが含まれているので session.user.idで取得できるようにしておく
+  callbacks: {
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = (token.id || token.sub || "") as string;
+        session.user.name = token.name ?? "";
+        session.user.email = token.email ?? "";
+      }
+      return session;
+    },
+  },
 });
